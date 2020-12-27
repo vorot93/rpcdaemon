0091278281712448968fa92acb7912ec502928cc
Artem Vorotnikov <artem@vorotnikov.me>
2020-12-27 23:07:46 +0300

    Fix CLI

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,10 @@
 use clap::Clap;
 
 #[derive(Clap, Debug)]
+#[clap(name = "tarpcdaemon", about = "RPC daemon for turbo-geth.")]
 pub struct Opts {
+    #[clap(long, env)]
     pub listen_addr: String,
+    #[clap(long, env)]
     pub kv_addr: String,
 }
